339c7379284ad22dc028cca11f4ecf67ddd72374
Imhyung Lee <rhdtn987@gmail.com>
2023-11-27 21:43:35 +0900

    냥

--- app.py
+++ app.py
@@ -54,75 +54,78 @@ def upload_file():
         
         #딕셔너리 정의(모델에서 딕셔너리 형태로 받을예정)
         grade=dict()
-        grade={'adress':40,'take_away':{'iron_parallel':20,'arm_stretch':70},'back_swing':{'head_arm':60,'hand_position':30},'impact':50,'finish':55}
+        grade={'address':1,'takeback':174,'backswing':170,'top':1,'impact_eye':-1,'impact_knee':1,'impact_foot':1}
         
         #코멘트 리스트. 나중에 웹으로 값 전송
         comment=list() 
-        
+
         #점수 리스트.
         sc=list[20,40,60,80,90,100,10]
         
+        #종합스코어
+        cnt=0
+        
         #코멘트 알고리즘, 점수는 50점일때 각이 일치하는걸 가정, 오차는 5
         for key,value in grade.items():
-            if(key=='adress'):
-                if(value<45):
+            if(key=='address'):
+                if(value==-1):
                     comment.append('발의 각도가 좁아요 어깨만큼 넓혀주세요ㅜㅜ.')
-                elif(value<=55 & value>=45):
+                elif(value==1 ):
                     comment.append('어드레스의 발 사이의 거리가 좋아요^^.')
-                elif(value>55):
+                    cnt+=1
+                elif(value==0):
                     comment.append('발 사이의 거리가 너무 멀어요ㅜㅜ.')
                     
-            elif(key=='take_away'): #채 수평(손목좌표)과 팔 쭉 펴진지 나중에
-                for inner_key,inner_value in value.items():
-                    
-                    if(inner_key=='iron_parallel'): # 채부분
-                        if(inner_value<45):
-                            comment.append('채가 아래로 향해 있어요. 채를 수평에 맞추어 주세요.')
-                        elif(inner_value<=55 & inner_value>=45):
-                            comment.append('채의 각도와 손목이 훌륭해요^^.')
-                        elif(inner_value>55):
-                            comment.append('채가 위로로 향해 있어요. 채를 수평에 맞추어 주세요.')
-                            
-                    #팔
-                    elif(inner_key=='arm_stretch'):                      
-                        if(inner_value<45):
-                            comment.append('팔을 쭉 펴주세요!')
-                        else:
-                            comment.append('팔이 잘 펴져있어요^^.')
-                          
+            elif(key=='takeback'): #채 수평(손목좌표)과 팔 쭉 펴진지 나중에
+                if(value==1):
+                    comment.append('테이크 어웨이 구간에서 팔이 잘 펴졌어요ㅎㅎ')
+                    cnt+=1
+                elif(value<165):
+                    comment.append('테이크 어웨이 구간에서 팔을 더 펴주세요ㅜㅜ')
+                   
+                 
             #뱌ㅐㄱ스윙
-            elif(key=='back_swing'): 
-                for inner_key,inner_value in value.items():
-                    
-                    #왼팔어깨와 머리 사이의 거리값?
-                    if(inner_key=='head_arm'):
-                        if(inner_value<45):
-                            comment.append('왼쪽 어깨가 얼굴 아래에 잘 부착 되있어요!')
-                        elif(inner_value>=45):
-                            comment.append('왼쪽 어깨를 얼굴아래로 부착시켜주세요!')
-                            
-                    #손의 위치기 너무 낮거나 머리를 넘어가면 안되는 부분
-                    elif(inner_key=='hand_position'):                      
-                        if(inner_value<45):
-                            comment.append('팔을 더 위로 올려주세요!.')
-                        elif(inner_value<=55 & inner_value>=45):
-                            comment.append('손의 위치기 적절해요.')
-                        elif(inner_value>55):
-                            comment.append('손이 머리를 넘어가지 않도록 주의 해주세요!.')
-                    
-            elif(key=='impact'): #고개 위치
-                if(value<=55 & value>=45):
+            elif(key=='backswing'): 
+                
+                if(value==1):
+                    comment.append('백스윙 구간에서 팔이 잘 펴졌어요ㅎㅎ')
+                    cnt+=1
+                elif(value<155):
+                    comment.append('백스윙 구간에서 팔을 더 펴주세요ㅜㅜ')
+                   
+            elif(key=='top'): #백스윙에서 팔 위치
+                if(value==1):
                     comment.append('볼을 끝까지 보는 시선 좋아요^^.')
-                else:
+                    cnt+=1
+                elif(value==0):
                     comment.append('임팩트에서 볼을 끝까지 봐야해요!.')
                     
-            #시간으로 값을 전달받을듯
-            elif(key=='finish'):
-                if(value<45):
-                    comment.append('피니쉬 자세를 좀 더 유지해 볼까요?')
-                else:
+            #임팩트
+            #임팩트 시선
+            elif(key=='impact_eye'):
+                if(value==1):
+                    comment.append('임팩트 시 시선처리 좋아요!')
+                    cnt+=1
+                elif(value==0):
+                    comment.append('임팩트때 공을 끝까지 봐주세요!')
+            
+            #임펙트 무릎
+            elif(key=='impact_knee'):
+                if(value==1):
                     comment.append('좋아요!')
-                       
+                    cnt+=1
+                elif(value==0):
+                    comment.append('임패트 시 무릎이 붙게 해주세요!')           
+                    
+            #임펙트 발거리
+            elif(key=='impact_foot'):
+                if(value==-1):
+                    comment.append('발의 각도가 좁아요 어깨만큼 넓혀주세요ㅜㅜ.')
+                elif(value==1):
+                    comment.append('어드레스의 발 사이의 거리가 좋아요^^.')
+                    cnt+=1
+                elif(value==0):
+                    comment.append('발 사이의 거리가 너무 멀어요ㅜㅜ.')
         #넘기는 리스트 comment의 값은 총 7개
             # 어드레스 한개
             # 테이크어웨이 두개(채가 지면과 평행,팔 쭉펴주기)
