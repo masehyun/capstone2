4aceb8c28d632f4fbb5718ae96d5c743bf7875d5
Hwangmyeongje <hmj1438@naver.com>
2023-11-27 22:57:58 +0900

    1

---
+++ model.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def model_f():
+  a=list()
+  for i in range(7):  
+    a.insert(i,randint(1,100))
+  return a
+    
+# a=model_f()
+
+# for i in range(len(a)):
+#   print(a[i])    
+
---
+++ score.py
@@ -0,0 +1,16 @@
+# 정답값이랑 결과값 두 가지가 와야되나?
+# 두가지가 온다면 dict={'step1_value'=50,'step1_correct'='70'}
+# 각도를 기준으로 하니까 
+# dict score=>sc
+
+grade={'1':1,'w':2}
+for k,v in grade.items():
+  print(k,v,'🏌️') 
+
+
+def evaluate(**sc):
+  feedback=list()
+  
+  
+
+  
